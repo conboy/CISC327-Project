2d1c8b978d4deabeeb5c5f733268326fb25af340
conrad <19cjf6@queensu.ca>
2022-09-22 17:24:07 -0400

    Created review model

---
+++ CISC327-Project/src/models/CreatingReviewModel.java
@@ -0,0 +1,15 @@
+package models;
+import java.util.Date;
+
+public class CreatingReviewModel {
+    private int reviewID;
+	private int reviewerID;
+	private int listingID;
+	private Date reviewDate;
+	private int reviewRating;
+	private String reviewBody;
+	
+	public CreatingReviewModel() {
+
+    }
+}
