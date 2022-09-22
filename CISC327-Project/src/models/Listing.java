c3bbcbc778dd9c65cc01c35cbde534310208533d
datmaxo <s2063614@ed.ac.uk>
2022-09-22 14:32:37 -0400

    Forgot to add file; listing class model created.

--- CISC327-Project/src/models/Listing.java
+++ CISC327-Project/src/models/Listing.java
@@ -1,2 +1,26 @@
-package models;public class Listing {
+package models;
+
+public class Listing {
+
+    private class Address {
+        public String line1;
+        public String line2;
+        public String city;
+        public String province;
+        public String postcode;
+
+        private Address() {
+
+        }
+    }
+
+    private int ownerID;
+    private Address listingAddress;
+    private int bedroomCount;
+    private int bedCount;
+    private double bathroomCount;
+
+    public Listing() {
+
+    }
 }
