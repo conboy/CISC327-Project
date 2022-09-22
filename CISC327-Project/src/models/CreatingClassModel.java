6c1208514ee34fa33697a91567cbc34223d59d9e
aidanleyne <aidan@leyne.com>
2022-09-21 20:23:13 -0400

    Update for a models directory in src

---
+++ CISC327-Project/src/models/CreatingClassModel.java
@@ -0,0 +1,8 @@
+package models;
+
+public class CreatingClassModel {
+
+	//Nothing to see here
+	//This should be deleted later.
+	//This class was only created so that the folder would be pushed.
+}
