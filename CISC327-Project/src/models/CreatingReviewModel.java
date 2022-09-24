fc0aaa202bfcbc0d8a83689558ab49176d523116
conrad <19cjf6@queensu.ca>
2022-09-23 20:01:40 -0400

    Formatted review model to Google style guide

--- CISC327-Project/src/models/CreatingReviewModel.java
+++ CISC327-Project/src/models/CreatingReviewModel.java
@@ -1,15 +1,14 @@
 package models;
-import java.util.Date;
+
+import java.util.Date; // For formating dates
 
 public class CreatingReviewModel {
-    private int reviewID;
-	private int reviewerID;
-	private int listingID;
-	private Date reviewDate;
-	private int reviewRating;
-	private String reviewBody;
-	
-	public CreatingReviewModel() {
+  private int reviewID;
+  private int reviewerID;
+  private int listingID;
+  private Date reviewDate;
+  private int reviewRating;
+  private String reviewBody;
 
-    }
+  public CreatingReviewModel() {}
 }
