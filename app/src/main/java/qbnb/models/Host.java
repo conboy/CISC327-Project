19b86697bd0ac3a6f01f87ac2ddcbf87fa0a7fae
Cain Susko <20cjbs@queensu.ca>
2022-10-06 13:07:14 -0400

    Restructure project
    restructures the existing code to work correctly within a newly created
    Java Gradle project.

---
+++ app/src/main/java/qbnb/models/Host.java
@@ -0,0 +1,12 @@
+package models;
+
+import java.util.ArrayList;
+
+/** The object used for a host as a user Child of the user class. */
+public class Host extends User {
+  private ArrayList<Listing> listings;
+  private ArrayList<Review> reviews;
+  private double accountBalance;
+
+  public Host() {}
+}
