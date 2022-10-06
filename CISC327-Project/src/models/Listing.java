28de02d3d2b49e0167d50a1906ff78d524b81f62
datmaxo <s2063614@ed.ac.uk>
2022-10-05 23:25:31 -0400

    Removed Address subclass

--- CISC327-Project/src/models/Listing.java
+++ CISC327-Project/src/models/Listing.java
@@ -4,32 +4,12 @@ import java.util.Date;
 
 public class Listing {
 
-    //Subclass to store different aspects of an address, as opposed to saving everything in one string
-    private class Address {
-        private String line1;
-        private String line2;
-        private String city;
-        private String province;
-        private String postcode;
-
-        /**
-         * Creates and initialises a new Address.
-         * TODO: Fully implement this method!
-         */
-        public Address() {
-
-        }
-    }
-
     private int listingID;
     private String title;
     private String description;
     private double price;
     private Date modificationDate;
     private int ownerID;
-
-    /* Currently unused variables - may be implemented in the future? */
-    //private Address listingAddress;
 
     /**
      * Creates and initialises a new Listing.
