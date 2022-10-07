4bd629df539d2b41cba0182d552652142a2dc8c3
datmaxo <s2063614@ed.ac.uk>
2022-10-07 01:54:25 -0400

    Refactored code to google formatting standard

--- app/src/main/java/qbnb/models/UserDao.java
+++ app/src/main/java/qbnb/models/UserDao.java
@@ -7,35 +7,36 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-/**  == TEMPORARY USERDAO IMPLEMENTATION ==
- *  used only to help handle listing requirements
- *  full implementation should be in features/327-24 */
+/**
+ * == TEMPORARY USERDAO IMPLEMENTATION == used only to help handle listing requirements full
+ * implementation should be in features/327-24
+ */
 public class UserDao implements Dao<User> {
 
-    private static List<User> users = new ArrayList<User>();
+  private static List<User> users = new ArrayList<User>();
 
-    @Override
-    public Optional<User> get(long id) {
-        return Optional.empty();
-    }
+  @Override
+  public Optional<User> get(long id) {
+    return Optional.empty();
+  }
 
-    @Override
-    public List<User> getAll() {
-        return users;
-    }
+  @Override
+  public List<User> getAll() {
+    return users;
+  }
 
-    @Override
-    public void save(User user) {
-        users.add(user);
-    }
+  @Override
+  public void save(User user) {
+    users.add(user);
+  }
 
-    @Override
-    public void update(User user, String[] params) {
-        //TODO
-    }
+  @Override
+  public void update(User user, String[] params) {
+    // TODO
+  }
 
-    @Override
-    public void delete(User user) {
-        //TODO
-    }
+  @Override
+  public void delete(User user) {
+    // TODO
+  }
 }
