304bc9ec2c9b8dcaede9dcbac8db3689a7f29a53
datmaxo <s2063614@ed.ac.uk>
2022-10-07 02:30:44 -0400

    Fixing compilation errors [attempt 2]

--- app/src/main/java/qbnb/models/Guest.java
+++ app/src/main/java/qbnb/models/Guest.java
@@ -3,8 +3,8 @@ package qbnb.models;
 import java.util.ArrayList;
 
 /** The object used for a guest as a user Child of the user class. */
-public class Guest extends User {
-  private ArrayList<Listing> bookings;
+public class Guest extends qbnb.models.User {
+  private ArrayList<qbnb.models.Listing> bookings;
   private ArrayList<Review> reviews;
   private double accountBalance;
 
--- app/src/main/java/qbnb/models/Host.java
+++ app/src/main/java/qbnb/models/Host.java
@@ -3,8 +3,8 @@ package qbnb.models;
 import java.util.ArrayList;
 
 /** The object used for a host as a user Child of the user class. */
-public class Host extends User {
-  private ArrayList<Listing> listings;
+public class Host extends qbnb.models.User {
+  private ArrayList<qbnb.models.Listing> listings;
   private ArrayList<Review> reviews;
   private double accountBalance;
 
