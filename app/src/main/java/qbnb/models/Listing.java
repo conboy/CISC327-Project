498cd08f080839b55854d56c79ba90205f6f5924
datmaxo <s2063614@ed.ac.uk>
2022-10-06 23:21:13 -0400

    Implemeted getter functions for Listing Reformatted indentation of Listing.

--- app/src/main/java/qbnb/models/Listing.java
+++ app/src/main/java/qbnb/models/Listing.java
@@ -11,49 +11,79 @@ import java.time.LocalDate;
  */
 public class Listing {
 
-  //The ID of the listing object - used to identify between individual listings.
-  private int listingID;
+    //The ID of the listing object - used to identify between individual listings.
+    private int listingID;
 
-  //The title of the listing that is shown to users. Alphanumeric only.
-  private String title;
+    //The title of the listing that is shown to users. Alphanumeric only.
+    private String title;
 
-  //The description of the listing that is shown to users prior to booking. Must be longer than the title!
-  private String description;
+    //The description of the listing that is shown to users prior to booking. Must be longer than the title!
+    private String description;
 
-  //The price of the listing per night. Can range from 10$ to 10,000$.
-  private double price;
+    //The price of the listing per night. Can range from 10$ to 10,000$.
+    private double price;
 
-  //The date of when the listing was last modified by its owner. Does not accept dates past 2025.
-  private LocalDate modificationDate;
+    //The date of when the listing was last modified by its owner. Does not accept dates past 2025.
+    private LocalDate modificationDate;
 
-  //The ID of the user who created the listing and owns the property. Used to reference the owner's info when necessary.
-  private int ownerID;
+    //The ID of the user who created the listing and owns the property. Used to reference the owner's info when necessary.
+    private int ownerID;
 
-  /**
-   * Creates and initialises a new Listing.
-   * If any of the requirements as established in Sprint #2 R4 are broken, an Illegal Argument exception is thrown.
-   */
-  public Listing(int id, String title, String description, double price, LocalDate modDate, int owner) {
-    this.listingID = id;
+    /**
+     * Creates and initialises a new Listing.
+     * If any of the requirements as established in Sprint #2 R4 are broken, an Illegal Argument exception is thrown.
+     */
+    public Listing(int id, String title, String description, double price, LocalDate modDate, int owner) {
+        this.listingID = id;
 
-    if (!title.matches("[a-zA-z0-9 ]+")) throw new IllegalArgumentException("R4-1");
-    else if (title.charAt(0) == ' ' || title.charAt(title.length() - 1) == ' ') throw new IllegalArgumentException("R4-1");
-    else if (title.length() > 80) throw new IllegalArgumentException("R4-2");
-    this.title = title;
+        if (!title.matches("[a-zA-z0-9 ]+")) throw new IllegalArgumentException("R4-1");
+        else if (title.charAt(0) == ' ' || title.charAt(title.length() - 1) == ' ') throw new IllegalArgumentException("R4-1");
+        else if (title.length() > 80) throw new IllegalArgumentException("R4-2");
+        this.title = title;
 
-    if (description.length() > 2000 || description.length() < 20) throw new IllegalArgumentException("R4-3");
-    else if (title.length() >= description.length()) throw new IllegalArgumentException("R4-4");
-    this.description = description;
+        if (description.length() > 2000 || description.length() < 20) throw new IllegalArgumentException("R4-3");
+        else if (title.length() >= description.length()) throw new IllegalArgumentException("R4-4");
+        this.description = description;
 
-    if (price < 10 || price > 10000) throw new IllegalArgumentException("R4-5");
-    this.price = price;
+        if (price < 10 || price > 10000) throw new IllegalArgumentException("R4-5");
+        this.price = price;
 
-    //not sure if this check is necessary - upon creation, shouldn't modification date be set to the current date?
-    if (modDate.isBefore(LocalDate.parse("2021-01-02")) || modDate.isAfter(LocalDate.parse("2025-01-02"))) throw new IllegalArgumentException("R4-6");
-    this.modificationDate = modDate;
+        //not sure if this check is necessary - upon creation, shouldn't modification date be set to the current date?
+        if (modDate.isBefore(LocalDate.parse("2021-01-02")) || modDate.isAfter(LocalDate.parse("2025-01-02"))) throw new IllegalArgumentException("R4-6");
+        this.modificationDate = modDate;
 
-    //requirements dictate owner email shouldn't be empty - since we're using an ID system, ID should be greater than zero!
-    if (owner < 1) throw new IllegalArgumentException("R4-7");
-    this.ownerID = owner;
-  }
+        //requirements dictate owner email shouldn't be empty - since we're using an ID system, ID should be greater than zero!
+        if (owner < 1) throw new IllegalArgumentException("R4-7");
+        this.ownerID = owner;
+    }
+
+    /** Returns listing ID */
+    public int getListingID () {
+        return listingID;
+    }
+
+    /** Returns title */
+    public String getTitle () {
+        return title;
+    }
+
+    /** Returns description */
+    public String getDescription () {
+        return description;
+    }
+
+    /** Returns price */
+    public double getPrice () {
+        return price;
+    }
+
+    /** Returns modification date */
+    public LocalDate getModificationDate () {
+        return modificationDate;
+    }
+
+    /** Returns owner ID */
+    public int getOwnerID () { 
+        return ownerID;
+    }
 }
