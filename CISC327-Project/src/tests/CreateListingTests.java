ef73840347299c293ec2fb6e4c7a8db0e401e8c9
datmaxo <s2063614@ed.ac.uk>
2022-10-06 21:53:54 -0400

    Implemented Tests R2 through to R-7A Further testing requires more detail in regards to database system.

--- CISC327-Project/src/tests/CreateListingTests.java
+++ CISC327-Project/src/tests/CreateListingTests.java
@@ -12,6 +12,7 @@ public class CreateListingTests {
     /**
      * Tests requirement R4-1: The title of the product has to be alphanumeric-only.
      * Additionally, spaces are allowed only if it is not as prefix and suffix.
+     * REQUIRES LISTING TO THROW ERRORS WITH MESSAGE 'R4-1' TO FUNCTION.
      */
     @Test
     public void alphanumericTitleTest () {
@@ -21,7 +22,7 @@ public class CreateListingTests {
             Listing x = new Listing(1,
                     "Quantum guy's £$@$!@£$$@! factory",
                     "A super fun factory of nonsense anc whimsy innit!",
-                    10, LocalDate.now(), 1);
+                    100, LocalDate.now(), 1);
         }
         catch (IllegalArgumentException e) {
             message1 = e.getMessage();
@@ -34,7 +35,7 @@ public class CreateListingTests {
             Listing x = new Listing(1,
                     "     Front space land",
                     "A super fun land of nonsense anc whimsy innit!",
-                    10, LocalDate.now(), 1);
+                    100, LocalDate.now(), 1);
         }
         catch (IllegalArgumentException e) {
             message2 = e.getMessage();
@@ -47,7 +48,7 @@ public class CreateListingTests {
             Listing x = new Listing(1,
                     "End space land    ",
                     "A super fun land of nonsense anc whimsy innit!",
-                    10, LocalDate.now(), 1);
+                    100, LocalDate.now(), 1);
         }
         catch (IllegalArgumentException e) {
             message3 = e.getMessage();
@@ -57,50 +58,160 @@ public class CreateListingTests {
 
     /**
      * Tests requirement R4-2: The title of the product is no longer than 80 characters.
+     * REQUIRES LISTING TO THROW ERRORS WITH MESSAGE 'R4-2' TO FUNCTION.
      */
     @Test
     public void titleLengthTest () {
-        Assert.fail();
+        String message = "";
+        try {
+            String desc = "a".repeat(200); //simple way of writing an arbitrarily long description
+            Listing x = new Listing(1,
+                    "The Really Really Long Named Place Of Absolute Joy And Wonder And Merriment And Amusement which is better known as The Ultimate Funkodome of the 31st Century",
+                    desc,
+                    100, LocalDate.now(), 1);
+        }
+        catch (IllegalArgumentException e) {
+            message = e.getMessage();
+        }
+        Assert.assertEquals("R4-2", message);
     }
 
     /**
      * Tests requirement R4-3: The description is minimum length of 20 characters and a maximum of 2000 characters.
+     * REQUIRES LISTING TO THROW ERRORS WITH MESSAGE 'R4-3' TO FUNCTION.
      */
     @Test
     public void descriptionLengthTest () {
-        Assert.fail();
+        //testing if error is thrown for the title length < 20 case - 'R4-3' is expected error message.
+        String message1 = "";
+        try {
+            Listing x = new Listing(1,
+                    "Funland",
+                    "It's funland!",
+                    100, LocalDate.now(), 1);
+        }
+        catch (IllegalArgumentException e) {
+            message1 = e.getMessage();
+        }
+        Assert.assertEquals("R4-3", message1);
+
+        //testing if error is thrown for the title length > 2000 case.
+        String message2 = "";
+        try {
+            String desc = "bee".repeat(1000); //simple way of writing an arbitrarily long description
+            Listing x = new Listing(1,
+                    "The entire bee movie script",
+                    desc,
+                    10, LocalDate.now(), 1);
+        }
+        catch (IllegalArgumentException e) {
+            message2 = e.getMessage();
+        }
+        Assert.assertEquals("R4-3", message2);
     }
 
     /**
      * Tests requirement R4-4: Description has to be longer than the product's title.
+     * REQUIRES LISTING TO THROW ERRORS WITH MESSAGE 'R4-4' TO FUNCTION.
      */
     @Test
     public void descriptionLongerThanTitleTest () {
-        Assert.fail();
+        String message = "";
+        try {
+            Listing x = new Listing(1,
+                    "Ultimate bogland",
+                    "it's a bog",
+                    100, LocalDate.now(), 1);
+        }
+        catch (IllegalArgumentException e) {
+            message = e.getMessage();
+        }
+        Assert.assertEquals("R4-4", message);
     }
 
     /**
      * Tests requirement R4-5: Price has to be within the range [10, 10000].
+     * REQUIRES LISTING TO THROW ERRORS WITH MESSAGE 'R4-5' TO FUNCTION.
      */
     @Test
     public void priceWithinRangeTest () {
-        Assert.fail();
+        //testing if correct error is thrown for the price < 10 case.
+        String message1 = "";
+        try {
+            Listing x = new Listing(1,
+                    "Funland",
+                    "It's funland!",
+                    1, LocalDate.now(), 1);
+        }
+        catch (IllegalArgumentException e) {
+            message1 = e.getMessage();
+        }
+        Assert.assertEquals("R4-5", message1);
+
+        //testing if error is thrown for the price > 10000 case.
+        String message2 = "";
+        try {
+            Listing x = new Listing(1,
+                    "Bunland",
+                    "It's fun on the bun! - Futurama robot man",
+                    10000000, LocalDate.now(), 1);
+        }
+        catch (IllegalArgumentException e) {
+            message2 = e.getMessage();
+        }
+        Assert.assertEquals("R4-5", message2);
     }
 
     /**
      * Tests requirement R4-6: modified_date must be after 2021-01-02 and before 2025-01-02.
+     * REQUIRES LISTING TO THROW ERRORS WITH MESSAGE 'R4-6' TO FUNCTION.
      */
     @Test
     public void dateWithinRangeTest () {
-        Assert.fail();
+        //testing if correct error is thrown for the Date < 2021-01-02 case.
+        String message1 = "";
+        try {
+            Listing x = new Listing(1,
+                    "Funland",
+                    "It's funland!",
+                    100, LocalDate.parse("2021-01-01"), 1);
+        }
+        catch (IllegalArgumentException e) {
+            message1 = e.getMessage();
+        }
+        Assert.assertEquals("R4-6", message1);
+
+        //testing if correct error is thrown for the Date > 2025-01-02 case.
+        String message2 = "";
+        try {
+            Listing x = new Listing(1,
+                    "Funland",
+                    "It's funland!",
+                    10, LocalDate.parse("2025-01-03"), 1);
+        }
+        catch (IllegalArgumentException e) {
+            message2 = e.getMessage();
+        }
+        Assert.assertEquals("R4-6", message2);
     }
 
     /**
      * Tests requirement R4-7A: ownerID cannot be empty.
+     * REQUIRES LISTING TO THROW ERRORS WITH MESSAGE 'R4-7' TO FUNCTION.
      */
     @Test
     public void ownerNonEmptyTest () {
-        Assert.fail();
+        String message = "";
+        try {
+            Listing x = new Listing(1,
+                    "Ultimate bogland",
+                    "it's a bog",
+                    100, LocalDate.now(), -1);
+        }
+        catch (IllegalArgumentException e) {
+            message = e.getMessage();
+        }
+        Assert.assertEquals("R4-7", message);
     }
 
     /**
