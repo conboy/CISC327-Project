e38816acef550f72178cf8968add5506f454e8a6
conrad <19cjf6@queensu.ca>
2022-10-11 20:34:22 -0400

    Resolved merge errors

--- app/src/main/java/qbnb/models/User.java
+++ app/src/main/java/qbnb/models/User.java
@@ -1,5 +1,8 @@
 package qbnb.models;
 
+import org.apache.commons.validator.routines.EmailValdator;
+
+
 /** Is the parent and base for any user on the platform */
 public class User {
   private int userID;
@@ -7,6 +10,7 @@ public class User {
   private String password;
   private String email;
 
+<<<<<<< HEAD
   // TEMPORARY CONSTRUCTOR - USED ONLY FOR LISTING IMPLEMENTATION PURPOSES
   public User(int ID, String user, String password, String email) {
     this.userID = ID;
@@ -22,4 +26,64 @@ public class User {
   public int getUserID() {
     return userID;
   }
+=======
+  public User() {}
+
+  public User(String email, String username, String password) {
+    this(email, username, passowrd, true);
+  }
+
+  public User(String email, String username, String Password, Boolean isGuest) {
+    if (checkEmail && checkUsername && checkPassowrd) {
+
+    }
+  }
+
+  /**
+   * 
+   */
+  private Boolean checkEmail(String email) {
+    return EmailValdator.getInstrance(true).isValid(email);
+  }
+
+  public Boolean updateEmail(String email) {
+    if (checkEmail(email)) {
+
+    }
+  }
+
+  public String getEmail() {
+    return this.email;
+  }
+
+  private Boolean checkUsername(String username) {
+    if (checkUsername(username)) {
+
+    }
+  }
+
+  public Boolean updateUsername(String username) {
+
+  }
+
+  public String getUsername() {
+    return this.username;
+  } 
+
+  private Boolean checkPassword(String password) {
+    if (checkPasword(password)) {
+
+    }
+  }
+
+  public Bolean updatePassword(String password) {
+
+  }
+
+  public String getPassowrd() {
+    return this.password;
+  }
+
+
+>>>>>>> e2be0dc328c7a90cae551d1bb7f54f68b1b772fd
 }
