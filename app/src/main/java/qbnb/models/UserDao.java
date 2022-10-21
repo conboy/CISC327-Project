0a20589e9af22ef4c78d87bfdaaa95a2047fb6c2
aidanleyne <aidan@leyne.com>
2022-10-20 22:27:10 -0400

    Updated UserDao for json

--- app/src/main/java/qbnb/models/UserDao.java
+++ app/src/main/java/qbnb/models/UserDao.java
@@ -1,44 +1,112 @@
 package qbnb.models;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import com.google.gson.Gson;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
 
 /**
- * == TEMPORARY USERDAO IMPLEMENTATION == used only to help handle listing requirements full
- * implementation should be in features/327-24
+ * Public interface for program when dealing with users
  */
-public class UserDao implements Dao<User> {
+public class UserDao {
 
-  private static List<User> users = new ArrayList<User>();
+  private HashMap<Long, User> users = new HashMap<Long, User>();
+  private final Gson gson = new Gson();
 
-  @Override
-  public Optional<User> get(long id) {
-    return Optional.ofNullable(users.get((int) id));
+  /** 
+   * Returns json copy of wanted user
+   * @param id - id of user to be returned
+   * @return String - json object of user
+   */
+  public String get(long id) {
+    return gson.toJson(users.get(id));
+  }
+    
+  /** 
+   * Adds a user to the dao
+   * 
+   * @param data - user data to be added
+   * @return Boolean - returns true if succeeds
+   */
+  public Boolean add(String data) {
+    User user = gson.fromJson(data, User.class);
+    this.users.put(user.getUserID(), user);
+    return false;
   }
 
-  @Override
-  public List<User> getAll() {
-    return users;
+  /** 
+   * Returns json copy of all users in dao
+   * 
+   * @return String - json file as a string
+   */
+  public String getAll() {
+    return gson.toJson(users);
   }
 
-  @Override
-  public void save(User user) {
-    users.add(user);
-  }
+  /** 
+   * Saves all users in the dao
+   * This will most likely happen at exit
+   * 
+   * @return Boolean - true if operation succeeds
+   * @throws IOException
+   */
+  public Boolean save() throws IOException {
+    FileWriter writer = new FileWriter("/db/users.json");
+    try {
+      writer.write(gson.toJson(users));
+      writer.close();
+    } catch (IOException e) {
+      return false;
+    }
 
-  @Override
-  public void update(User user, String[] params) {
-    // TODO
+    return true;
   }
+  
+  /** 
+   * Updates a given user
+   * 
+   * @param id - id of user
+   * @param data - json data to be used
+   * @return Boolean
+   */
+  public Boolean update(Long id, String data) {
+    if(this.users.get(id) != null) {
+      this.users.put(id, gson.fromJson(data, User.class));
+      return true;
+    }
 
-  @Override
-  public void delete(User user) {
-    // TODO
+    return false;
   }
+  
+  /** 
+   * Deletes a given user from the user list
+   * 
+   * @param id - id of user
+   * @return Boolean - true if user is deleted
+   */
+  public Boolean delete(Long id) {
+    if(this.users.get(id) != null) {
+      this.users.remove(id);
+    }
 
-  /** Used to clear the DAO at the end of testing */
-  public void deleteAll() {
-    users = new ArrayList<User>();
+    return false;
+  }
+ 
+  /** 
+   * deletes all users kept in dao
+   * 
+   * @return Boolean - true if operation succeeds
+   */
+  public Boolean deleteAll() {
+    try {
+      save();
+    } catch (IOException e) {
+      return false;
+    }
+
+    this.users = new HashMap<Long, User>();
+
+    return true;    
   }
 }
