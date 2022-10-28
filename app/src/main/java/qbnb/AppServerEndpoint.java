0561430fe7791f1d23cc65be0c6b7a86b28a1166
Conrad Fernandez <conjamalex@gmail.com>
2022-10-28 04:22:03 -0400

    Implemented Java Server

--- app/src/main/java/qbnb/AppServerEndpoint.java
+++ app/src/main/java/qbnb/AppServerEndpoint.java
@@ -1,13 +1,16 @@
 package qbnb;
 
 import java.util.logging.Logger;
-import javax.websocket.CloseReason;
-import javax.websocket.OnClose;
-import javax.websocket.OnMessage;
-import javax.websocket.OnOpen;
-import javax.websocket.Session;
+import jakarta.websocket.CloseReason;
+import jakarta.websocket.OnClose;
+import jakarta.websocket.OnMessage;
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.Session;
+import jakarta.websocket.server.ServerEndpoint;
 
+@ServerEndpoint(value = "/game")
 public class AppServerEndpoint {
+
   private Logger logger = Logger.getLogger(this.getClass().getName());      
 
   @OnOpen
@@ -17,13 +20,30 @@ public class AppServerEndpoint {
 
   @OnMessage
   public String onMessage(String msg, Session session) {
-    logger.info("Message ... " + msg);
 
-    return "Server received: " + msg;
+    logger.info("Message ... " + msg);
+    String msgType = getMsgType(msg);
+    String arr[] = msg.split(":");
+
+    switch (msgType) {
+      case "register":
+        // TODO: What happens when register is called
+        // Creates user object and shows user what they typed in text fields in alert dialog on web
+        // app
+        return "email: " + " pass: ";
+      default:
+        return "Failed";
+    }
   }
 
   @OnClose
   public void onClose(Session session, CloseReason closeReason) {
     logger.info(String.format("Session %s closed because of %s", session.getId(), closeReason));
+  }
+
+  // Gets the desired action from the message sent from websocket
+  public String getMsgType(String msg) {
+    String[] msgArray = msg.split(":");
+    return msgArray[0];
   }
 }
