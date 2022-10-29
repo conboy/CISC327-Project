b5e8da7f6988c32dc0e258c9ca02d8aa829f8a39
aidanleyne <aidan@leyne.com>
2022-10-28 20:59:13 -0400

    fixed formatting according to style

--- app/src/main/java/qbnb/models/UserDao.java
+++ app/src/main/java/qbnb/models/UserDao.java
@@ -1,31 +1,29 @@
 package qbnb.models;
 
 import com.google.gson.Gson;
-
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 
-/**
- * Public interface for program when dealing with users
- */
+/** Public interface for program when dealing with users */
 public class UserDao {
 
   private final Gson gson = new Gson();
   private HashMap<Long, User> users = new HashMap<Long, User>();
 
-  /** 
+  /**
    * Returns json copy of wanted user
+   *
    * @param id - id of user to be returned
    * @return String - json object of user
    */
   public String get(long id) {
     return gson.toJson(users.get(id));
   }
-    
-  /** 
+
+  /**
    * Adds a user to the dao
-   * 
+   *
    * @param data - user data to be added
    * @return Boolean - returns true if succeeds
    */
@@ -35,19 +33,18 @@ public class UserDao {
     return false;
   }
 
-  /** 
+  /**
    * Returns json copy of all users in dao
-   * 
+   *
    * @return String - json file as a string
    */
   public String getAll() {
     return gson.toJson(users);
   }
 
-  /** 
-   * Saves all users in the dao
-   * This will most likely happen at exit
-   * 
+  /**
+   * Saves all users in the dao This will most likely happen at exit
+   *
    * @return Boolean - true if operation succeeds
    * @throws IOException
    */
@@ -62,40 +59,40 @@ public class UserDao {
 
     return true;
   }
-  
-  /** 
+
+  /**
    * Updates a given user
-   * 
+   *
    * @param id - id of user
    * @param data - json data to be used
    * @return Boolean
    */
   public Boolean update(Long id, String data) {
-    if(this.users.get(id) != null) {
+    if (this.users.get(id) != null) {
       this.users.put(id, gson.fromJson(data, User.class));
       return true;
     }
 
     return false;
   }
-  
-  /** 
+
+  /**
    * Deletes a given user from the user list
-   * 
+   *
    * @param id - id of user
    * @return Boolean - true if user is deleted
    */
   public Boolean delete(Long id) {
-    if(this.users.get(id) != null) {
+    if (this.users.get(id) != null) {
       this.users.remove(id);
     }
 
     return false;
   }
- 
-  /** 
+
+  /**
    * deletes all users kept in dao
-   * 
+   *
    * @return Boolean - true if operation succeeds
    */
   public Boolean deleteAll() {
@@ -107,12 +104,12 @@ public class UserDao {
 
     this.users = new HashMap<Long, User>();
 
-    return true;    
+    return true;
   }
 
-  /** 
+  /**
    * Converts Json from a string to the working user hashmap data structure
-   * 
+   *
    * @param data - data to be deserialized
    */
   public void deserialize(String data) {
