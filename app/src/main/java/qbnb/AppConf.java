0b623ae2809617e59f365f2057e64c3ac2c4147c
datmaxo <s2063614@ed.ac.uk>
2022-11-02 17:58:34 -0400

    Undid erroneous changes to AppConf

--- app/src/main/java/qbnb/AppConf.java
+++ app/src/main/java/qbnb/AppConf.java
@@ -10,8 +10,8 @@ package qbnb;
 
 public class AppConf {
   public static final String PROJECT_PATH =
-      System.getProperty("user.home") + "exchange/CISC327/CISC327-Project";
-  public static final String WIN_PROJECT_PATH = "/Users/datmaxo/exchange/CISC327/CISC327_Project";
+      System.getProperty("user.home") + "/work/CISC327-Project/CISC327-Project";
+  public static final String WIN_PROJECT_PATH = "D:/a/CISC327-Project/CISC327-Project";
   public static final String TRANSACTION_PATH = "/db/TransactionDao.json";
   public static final String TEST_PATH = "/db/test.json";
 }
