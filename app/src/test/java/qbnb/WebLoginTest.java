d4da08a2bb5d48cdd46278daaa610b7e549e7f63
Conrad Fernandez <conjamalex@gmail.com>
2022-11-10 16:32:37 -0500

    Finished Tests

--- app/src/test/java/qbnb/WebLoginTest.java
+++ app/src/test/java/qbnb/WebLoginTest.java
@@ -11,126 +11,133 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebLoginTest {
-    WebDriver driver;
-    boolean isLogin = false;
-    String baseUrl = WIN_PROJECT_PATH + "\\app\\src\\main\\js\\qbnb\\register.html";
+  WebDriver driver;
+  boolean isLogin = false;
+  String baseUrl = WIN_PROJECT_PATH + "\\app\\src\\main\\js\\qbnb\\register.html";
+  String loginUrl = WIN_PROJECT_PATH + "\\app\\src\\main\\js\\qbnb\\login.html";
 
-    @BeforeAll
-    static void setupClass() {
-        WebDriverManager.chromedriver().setup();
+  @BeforeAll
+  static void setupClass() {
+    WebDriverManager.chromedriver().setup();
+  }
+
+  @BeforeEach
+  void setupTest() {
+    try {
+      driver = new ChromeDriver();
+      driver.get(baseUrl);
+    } catch (Exception e) {
+
     }
+  }
 
-    @BeforeEach
-    void setupTest() {
-        try {
-            driver = new ChromeDriver();
-            driver.get(baseUrl);
-        } catch (Exception e) {
+  @AfterEach
+  void teardown() {
 
-        }
+    try {
+      driver.quit();
+    } catch (Exception e) {
+
     }
+  }
 
-    @AfterEach
-    void teardown() {
+  @Test
+  void validRegisterTest() throws InterruptedException {
 
-        try {
-            driver.quit();
-        } catch (Exception e) {
+    try {
+      // Create user
+      WebElement email = driver.findElement(By.id("email"));
+      WebElement username = driver.findElement(By.id("user"));
+      WebElement password = driver.findElement(By.id("pass"));
+      WebElement submit = driver.findElement(By.id("submit"));
+      email.sendKeys("johndoe@test.com");
+      username.sendKeys("JohnDoe");
+      password.sendKeys("JohnDoe123#");
+      submit.click();
+      sleep(1000);
+      driver.switchTo().alert().dismiss();
+      // Login
+      driver.get(loginUrl);
+      sleep(1000);
+      email = driver.findElement(By.id("email"));
+      password = driver.findElement(By.id("pass"));
+      submit = driver.findElement(By.id("submit"));
+      email.sendKeys("johndoe@test.com");
+      password.sendKeys("JohnDoe123#");
+      submit.click();
+      sleep(1000);
+      String alert = driver.switchTo().alert().getText();
+      if (alert.equals("Logged in successfully")) isLogin = true;
 
-        }
+    } catch (Exception e) {
+
     }
+    Assertions.assertTrue(isLogin);
+  }
 
-    @Test
-    void validRegisterTest() throws InterruptedException {
-        try {
-            // Create user
-            WebElement email = driver.findElement(By.id("email"));
-            WebElement username = driver.findElement(By.id("user"));
-            WebElement password = driver.findElement(By.id("pass"));
-            WebElement submit = driver.findElement(By.id("submit"));
-            email.sendKeys("johndoe@test.com");
-            username.sendKeys("JohnDoe");
-            password.sendKeys("JohnDoe123#");
-            submit.click();
-            sleep(1000);
+  @Test
+  void invalidEmailTest() throws InterruptedException {
+    try {
+      // Create user
+      WebElement email = driver.findElement(By.id("email"));
+      WebElement username = driver.findElement(By.id("user"));
+      WebElement password = driver.findElement(By.id("pass"));
+      WebElement submit = driver.findElement(By.id("submit"));
+      email.sendKeys("johndoe@test.com");
+      username.sendKeys("JohnDoe");
+      password.sendKeys("JohnDoe123#");
+      submit.click();
+      sleep(1000);
+      driver.switchTo().alert().dismiss();
+      // Login
+      driver.get(loginUrl);
+      sleep(1000);
+      email = driver.findElement(By.id("email"));
+      password = driver.findElement(By.id("pass"));
+      submit = driver.findElement(By.id("submit"));
+      email.sendKeys("asd@test.com");
+      password.sendKeys("JohnDoe123#");
+      submit.click();
+      sleep(1000);
+      String alert = driver.switchTo().alert().getText();
+      if (alert.equals("Logged in successfully")) isLogin = true;
 
-            // Login
-            driver.findElement(By.linkText("Log in")).click();
-            email = driver.findElement(By.id("email"));
-            password = driver.findElement(By.id("pass"));
-            submit = driver.findElement(By.id("submit"));
-            email.sendKeys("johndoe@test.com");
-            password.sendKeys("JohnDoe123#");
-            submit.click();
-            sleep(1000);
-            String alert = driver.switchTo().alert().getText();
-            if (alert.equals("Logged in successfully")) isLogin = true;
-            Assertions.assertTrue(isLogin);
-        } catch (Exception e) {
+    } catch (Exception e) {
 
-        }
     }
+    Assertions.assertFalse(isLogin);
+  }
 
-    @Test
-    void invalidEmailTest() throws InterruptedException {
-        try {
-            WebElement email = driver.findElement(By.id("email"));
-            WebElement username = driver.findElement(By.id("user"));
-            WebElement password = driver.findElement(By.id("pass"));
-            WebElement submit = driver.findElement(By.id("submit"));
-            email.sendKeys("johndoe");
-            username.sendKeys("JohnDoe");
-            password.sendKeys("JohnDoe123#");
-            submit.click();
-            sleep(1000);
-            String alert = driver.switchTo().alert().getText();
-            if (alert.equals("Success")) isLogin = true;
+  @Test
+  void invalidPassTest() throws InterruptedException {
+    try {
+      // Create user
+      WebElement email = driver.findElement(By.id("email"));
+      WebElement username = driver.findElement(By.id("user"));
+      WebElement password = driver.findElement(By.id("pass"));
+      WebElement submit = driver.findElement(By.id("submit"));
+      email.sendKeys("johndoe@test.com");
+      username.sendKeys("JohnDoe");
+      password.sendKeys("JohnDoe123#");
+      submit.click();
+      sleep(1000);
+      driver.switchTo().alert().dismiss();
+      // Login
+      driver.get(loginUrl);
+      sleep(1000);
+      email = driver.findElement(By.id("email"));
+      password = driver.findElement(By.id("pass"));
+      submit = driver.findElement(By.id("submit"));
+      email.sendKeys("johndoe@test.com");
+      password.sendKeys("dsf#");
+      submit.click();
+      sleep(1000);
+      String alert = driver.switchTo().alert().getText();
+      if (alert.equals("Logged in successfully")) isLogin = true;
 
-        } catch (Exception e) {
+    } catch (Exception e) {
 
-        }
-        Assertions.assertFalse(isLogin);
     }
-
-    @Test
-    void invalidUserTest() throws InterruptedException {
-
-        try {
-            WebElement email = driver.findElement(By.id("email"));
-            WebElement username = driver.findElement(By.id("user"));
-            WebElement password = driver.findElement(By.id("pass"));
-            WebElement submit = driver.findElement(By.id("submit"));
-            email.sendKeys("johndoe@test.com");
-            username.sendKeys("j");
-            password.sendKeys("JohnDoe123#");
-            submit.click();
-            sleep(1000);
-            String alert = driver.switchTo().alert().getText();
-            if (alert.equals("Success")) isLogin = true;
-            Assertions.assertFalse(isLogin);
-        } catch (Exception e) {
-
-        }
-    }
-
-    @Test
-    void invalidPassTest() throws InterruptedException {
-
-        try {
-            WebElement email = driver.findElement(By.id("email"));
-            WebElement username = driver.findElement(By.id("user"));
-            WebElement password = driver.findElement(By.id("pass"));
-            WebElement submit = driver.findElement(By.id("submit"));
-            email.sendKeys("johndoe@test.com");
-            username.sendKeys("JohnDoe");
-            password.sendKeys("JohnDoe12");
-            submit.click();
-            sleep(1000);
-            String alert = driver.switchTo().alert().getText();
-            if (alert.equals("Success")) isLogin = true;
-            Assertions.assertFalse(isLogin);
-        } catch (Exception e) {
-
-        }
-    }
+    Assertions.assertFalse(isLogin);
+  }
 }
