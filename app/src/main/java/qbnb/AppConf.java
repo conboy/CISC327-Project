70982f05386000d804992caeeef773db7881dc9e
Conrad Fernandez <conjamalex@gmail.com>
2022-11-10 14:06:51 -0500

    Solving OS issues

--- app/src/main/java/qbnb/AppConf.java
+++ app/src/main/java/qbnb/AppConf.java
@@ -11,7 +11,6 @@ package qbnb;
 public class AppConf {
   public static final String PROJECT_PATH =
       System.getProperty("user.home") + "/work/CISC327-Project/CISC327-Project";
-
   public static final String WIN_PROJECT_PATH = "D:/a/CISC327-Project/CISC327-Project";
   public static final String TRANSACTION_PATH = "/db/TransactionDao.json";
   public static final String TEST_PATH = "/db/test.json";
