d367b38940e45affb2467780e19131b1b9a6e364
Conrad Fernandez <conjamalex@gmail.com>
2022-11-10 15:03:47 -0500

    Trying over

--- app/src/test/java/qbnb/WebRegisterTest.java
+++ app/src/test/java/qbnb/WebRegisterTest.java
@@ -1,9 +1,9 @@
 package qbnb;
 
 import static java.lang.Thread.sleep;
-import static qbnb.AppConf.PROJECT_PATH;
 import static qbnb.AppConf.WIN_PROJECT_PATH;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,40 +11,88 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebRegisterTest {
-  public String path;
-  public boolean isLogin = false;
+  WebDriver driver;
+  boolean isLogin = false;
+  String baseUrl = WIN_PROJECT_PATH + "\\app\\src\\main\\js\\qbnb\\register.html";
+
+  @BeforeAll
+  static void setupClass() {
+    WebDriverManager.chromedriver().setup();
+  }
+
+  @BeforeEach
+  void setupTest() {
+    driver = new ChromeDriver();
+    driver.get(baseUrl);
+  }
+
+  @AfterEach
+  void teardown() {
+    driver.quit();
+  }
 
   @Test
   void validRegisterTest() throws InterruptedException {
-    String osCheck = System.getProperty("os.name").split(" ")[0];
-    if (osCheck.equals("Mac") || osCheck.equals("Windows")) {
-      AppThread thread = new AppThread();
-      thread.start();
-      String baseUrl;
-      if (osCheck.equals("Mac")) {
-        path = PROJECT_PATH;
-        System.setProperty("webdriver.chrome.driver", path + "/chromedriver");
-        baseUrl = path + "/app/src/main/js/qbnb/register.html";
-      } else {
-        path = WIN_PROJECT_PATH;
-        System.setProperty("webdriver.chrome.driver", path + "/chromedriver.exe");
-        baseUrl = "file://" + path + "\\app\\src\\main\\js\\qbnb\\register.html";
-      }
 
-      WebDriver driver = new ChromeDriver();
-      driver.get(baseUrl);
-      WebElement email = driver.findElement(By.id("email"));
-      WebElement username = driver.findElement(By.id("user"));
-      WebElement password = driver.findElement(By.id("pass"));
-      WebElement submit = driver.findElement(By.id("submit"));
-      email.sendKeys("johndoe@test.com");
-      username.sendKeys("JohnDoe");
-      password.sendKeys("JohnDoe123#");
-      submit.click();
-      sleep(1000);
-      String alert = driver.switchTo().alert().getText();
-      if (alert.equals("Success")) isLogin = true;
-      Assertions.assertTrue(isLogin);
-    }
+    WebElement email = driver.findElement(By.id("email"));
+    WebElement username = driver.findElement(By.id("user"));
+    WebElement password = driver.findElement(By.id("pass"));
+    WebElement submit = driver.findElement(By.id("submit"));
+    email.sendKeys("johndoe@test.com");
+    username.sendKeys("JohnDoe");
+    password.sendKeys("JohnDoe123#");
+    submit.click();
+    sleep(1000);
+    String alert = driver.switchTo().alert().getText();
+    if (alert.equals("Success")) isLogin = true;
+    Assertions.assertTrue(isLogin);
+  }
+
+  @Test
+  void invalidEmailTest() throws InterruptedException {
+    WebElement email = driver.findElement(By.id("email"));
+    WebElement username = driver.findElement(By.id("user"));
+    WebElement password = driver.findElement(By.id("pass"));
+    WebElement submit = driver.findElement(By.id("submit"));
+    email.sendKeys("johndoe");
+    username.sendKeys("JohnDoe");
+    password.sendKeys("JohnDoe123#");
+    submit.click();
+    sleep(1000);
+    String alert = driver.switchTo().alert().getText();
+    if (alert.equals("Success")) isLogin = true;
+    Assertions.assertFalse(isLogin);
+  }
+
+  @Test
+  void invalidUserTest() throws InterruptedException {
+    WebElement email = driver.findElement(By.id("email"));
+    WebElement username = driver.findElement(By.id("user"));
+    WebElement password = driver.findElement(By.id("pass"));
+    WebElement submit = driver.findElement(By.id("submit"));
+    email.sendKeys("johndoe@test.com");
+    username.sendKeys("j");
+    password.sendKeys("JohnDoe123#");
+    submit.click();
+    sleep(1000);
+    String alert = driver.switchTo().alert().getText();
+    if (alert.equals("Success")) isLogin = true;
+    Assertions.assertFalse(isLogin);
+  }
+
+  @Test
+  void invalidPassTest() throws InterruptedException {
+    WebElement email = driver.findElement(By.id("email"));
+    WebElement username = driver.findElement(By.id("user"));
+    WebElement password = driver.findElement(By.id("pass"));
+    WebElement submit = driver.findElement(By.id("submit"));
+    email.sendKeys("johndoe@test.com");
+    username.sendKeys("JohnDoe");
+    password.sendKeys("JohnDoe12");
+    submit.click();
+    sleep(1000);
+    String alert = driver.switchTo().alert().getText();
+    if (alert.equals("Success")) isLogin = true;
+    Assertions.assertFalse(isLogin);
   }
 }
