0508f861476985cd753e27c4b60523e5fdaef642
Conrad Fernandez <conjamalex@gmail.com>
2022-11-11 16:46:11 -0500

    Comments

--- app/src/test/java/qbnb/WebRegisterTest.java
+++ app/src/test/java/qbnb/WebRegisterTest.java
@@ -10,6 +10,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+/**
+ * Runs test on the function Register function on the frontend and backend These tests check if the
+ * function did these correctly: R1-1: Email cannot be empty. password cannot be empty. R1-3: The
+ * email has to follow addr-spec defined in RFC 5322 R1-4: Password has to meet the required
+ * complexity: minimum length 6, at least one upper case, at least one lower case, and at least one
+ * special character. R1-5: User name has to be non-empty, alphanumeric-only, and space allowed only
+ * if it is not as the prefix or suffix. R1-6: User name has to be longer than 2 characters and less
+ * than 20 characters.
+ */
 public class WebRegisterTest {
   WebDriver driver;
   boolean isLogin = false;
@@ -37,12 +46,14 @@ public class WebRegisterTest {
   void teardown() {
 
     try {
-      driver.quit();
+      // driver.quit();
     } catch (Exception e) {
 
     }
   }
-  /** Test register with a valid email user and pass */
+  /**
+   * Test register functionality. Pass correct email user and password through Register function.
+   */
   @Test
   void validRegisterTest() throws InterruptedException {
     try {
@@ -62,7 +73,35 @@ public class WebRegisterTest {
 
     }
   }
-  /** Test an invalid email address */
+
+  /**
+   * Test R1-1: Email cannot be empty. password cannot be empty. Try to register with an empty email
+   * and password if the Register() returns false, test is passed.
+   */
+  @Test
+  void emptyTest() throws InterruptedException {
+
+    try {
+      WebElement email = driver.findElement(By.id("email"));
+      WebElement username = driver.findElement(By.id("user"));
+      WebElement password = driver.findElement(By.id("pass"));
+      WebElement submit = driver.findElement(By.id("submit"));
+      email.sendKeys("");
+      username.sendKeys("JohnDoe");
+      password.sendKeys("");
+      submit.click();
+      sleep(1000);
+      String alert = driver.switchTo().alert().getText();
+      if (alert.equals("Success")) isLogin = true;
+      Assertions.assertFalse(isLogin);
+    } catch (Exception e) {
+
+    }
+  }
+  /**
+   * Test R1-3: The email has to follow addr-spec defined in RFC 5322. Try to login with an invalid
+   * email address.
+   */
   @Test
   void invalidEmailTest() throws InterruptedException {
 
@@ -71,8 +110,8 @@ public class WebRegisterTest {
       WebElement username = driver.findElement(By.id("user"));
       WebElement password = driver.findElement(By.id("pass"));
       WebElement submit = driver.findElement(By.id("submit"));
-      email.sendKeys("johndoe");
-      username.sendKeys("JohnDoe");
+      email.sendKeys("johndoe.com");
+      username.sendKeys("johndoe");
       password.sendKeys("JohnDoe123#");
       submit.click();
       sleep(1000);
@@ -83,7 +122,10 @@ public class WebRegisterTest {
 
     }
   }
-  /** Test an invalid username */
+  /**
+   * R1-5: User name has to be non-empty, alphanumeric-only, and space allowed only if it is not as
+   * the prefix or suffix. Try to login with an invalid user name.
+   */
   @Test
   void invalidUserTest() throws InterruptedException {
 
@@ -93,8 +135,8 @@ public class WebRegisterTest {
       WebElement password = driver.findElement(By.id("pass"));
       WebElement submit = driver.findElement(By.id("submit"));
       email.sendKeys("johndoe@test.com");
-      username.sendKeys("j");
-      password.sendKeys("JohnDoe123#");
+      username.sendKeys("(*!&@#*()");
+      password.sendKeys("JohnDoe12");
       submit.click();
       sleep(1000);
       String alert = driver.switchTo().alert().getText();
@@ -104,9 +146,12 @@ public class WebRegisterTest {
 
     }
   }
-  /** Test an invalid password */
+  /**
+   * R1-6: User name has to be longer than 2 characters. Try to login with a user name shorter than
+   * 2 characters
+   */
   @Test
-  void invalidPassTest() throws InterruptedException {
+  void shortUserTest() throws InterruptedException {
 
     try {
       WebElement email = driver.findElement(By.id("email"));
@@ -114,7 +159,7 @@ public class WebRegisterTest {
       WebElement password = driver.findElement(By.id("pass"));
       WebElement submit = driver.findElement(By.id("submit"));
       email.sendKeys("johndoe@test.com");
-      username.sendKeys("JohnDoe");
+      username.sendKeys("a");
       password.sendKeys("JohnDoe12");
       submit.click();
       sleep(1000);
