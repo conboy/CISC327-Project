bb6ef4b35c8fe26f086c786da855e2e86dceb8f5
datmaxo <s2063614@ed.ac.uk>
2022-11-22 10:49:04 -0500

    Removed OS filtering on injection tests

--- app/src/test/java/qbnb/CreateListingInjectionTests.java
+++ app/src/test/java/qbnb/CreateListingInjectionTests.java
@@ -32,112 +32,103 @@ public class CreateListingInjectionTests {
 
   @Test
   public void titleInjectionTest() throws FileNotFoundException {
-    String osCheck = System.getProperty("os.name").split(" ")[0];
-    if (osCheck.equals("Mac") || osCheck.equals("Windows")) {
-      File x = new File(formatForSystem("/db/Generic_SQLI.txt"));
-      Scanner reader = new Scanner(x);
-      int id = 10001;
+    File x = new File(formatForSystem("/db/Generic_SQLI.txt"));
+    Scanner reader = new Scanner(x);
+    int id = 10001;
 
-      while (reader.hasNextLine()) {
-        String payload = reader.nextLine();
-        System.out.println(payload);
-        String prediction = null;
-        String error = null;
+    while (reader.hasNextLine()) {
+      String payload = reader.nextLine();
+      System.out.println(payload);
+      String prediction = null;
+      String error = null;
 
-        // predict if the payload will throw an error due to current input sanitization.
-        // if the prediction and the actual result do not match, it could be a sign of injection
-        // succeeding.
-        if (!payload.matches("[a-zA-z0-9 ]+")) prediction = "R4-1";
-        else if (payload.charAt(0) == ' ' || payload.charAt(payload.length() - 1) == ' ')
-          prediction = "R4-1";
-        else if (payload.length() > 80) prediction = "R4-2";
-        try {
-          Listing pListing =
-              new Listing(
-                  id,
-                  payload,
-                  "A really long description to ensure that R4-4 does not occur :) - ".repeat(5),
-                  100,
-                  LocalDate.now(),
-                  404);
-        } catch (IllegalArgumentException e) {
-          error = e.getMessage();
-        }
-        Assertions.assertEquals(prediction, error);
-        id++;
+      // predict if the payload will throw an error due to current input sanitization.
+      // if the prediction and the actual result do not match, it could be a sign of injection
+      // succeeding.
+      if (!payload.matches("[a-zA-z0-9 ]+")) prediction = "R4-1";
+      else if (payload.charAt(0) == ' ' || payload.charAt(payload.length() - 1) == ' ')
+        prediction = "R4-1";
+      else if (payload.length() > 80) prediction = "R4-2";
+      try {
+        Listing pListing =
+            new Listing(
+                id,
+                payload,
+                "A really long description to ensure that R4-4 does not occur :) - ".repeat(5),
+                100,
+                LocalDate.now(),
+                404);
+      } catch (IllegalArgumentException e) {
+        error = e.getMessage();
       }
-    } else Assertions.assertTrue(true);
+      Assertions.assertEquals(prediction, error);
+      id++;
+    }
   }
 
   @Test
   public void descriptionInjectionTest() throws FileNotFoundException {
-    String osCheck = System.getProperty("os.name").split(" ")[0];
-    if (osCheck.equals("Mac") || osCheck.equals("Windows")) {
-      File x = new File(formatForSystem("/db/Generic_SQLI.txt"));
-      Scanner reader = new Scanner(x);
-      int id = 20001;
+    File x = new File(formatForSystem("/db/Generic_SQLI.txt"));
+    Scanner reader = new Scanner(x);
+    int id = 20001;
 
-      while (reader.hasNextLine()) {
-        String payload = reader.nextLine();
-        System.out.println(payload);
-        String prediction = null;
-        String error = null;
+    while (reader.hasNextLine()) {
+      String payload = reader.nextLine();
+      System.out.println(payload);
+      String prediction = null;
+      String error = null;
 
-        // predict if the payload will throw an error due to current input sanitization.
-        if (payload.length() > 2000 || payload.length() < 20) prediction = "R4-3";
-        else if (Integer.toString(id).length() >= payload.length()) prediction = "R4-4";
-        try {
-          Listing pListing =
-              new Listing(id, Integer.toString(id), payload, 100, LocalDate.now(), 404);
-        } catch (IllegalArgumentException e) {
-          error = e.getMessage();
-        }
-        Assertions.assertEquals(prediction, error);
-        id++;
+      // predict if the payload will throw an error due to current input sanitization.
+      if (payload.length() > 2000 || payload.length() < 20) prediction = "R4-3";
+      else if (Integer.toString(id).length() >= payload.length()) prediction = "R4-4";
+      try {
+        Listing pListing =
+            new Listing(id, Integer.toString(id), payload, 100, LocalDate.now(), 404);
+      } catch (IllegalArgumentException e) {
+        error = e.getMessage();
       }
-    } else Assertions.assertTrue(true);
+      Assertions.assertEquals(prediction, error);
+      id++;
+    }
   }
 
   @Test
   public void priceInjectionTest() throws FileNotFoundException {
-    String osCheck = System.getProperty("os.name").split(" ")[0];
-    if (osCheck.equals("Mac") || osCheck.equals("Windows")) {
-      File x = new File(formatForSystem("/db/Generic_SQLI.txt"));
-      Scanner reader = new Scanner(x);
-      int id = 30001;
+    File x = new File(formatForSystem("/db/Generic_SQLI.txt"));
+    Scanner reader = new Scanner(x);
+    int id = 30001;
 
-      while (reader.hasNextLine()) {
-        String payload = reader.nextLine();
-        System.out.println(payload);
-        String prediction = null;
-        String error = null;
+    while (reader.hasNextLine()) {
+      String payload = reader.nextLine();
+      System.out.println(payload);
+      String prediction = null;
+      String error = null;
 
-        // predict if the payload will throw an error due to current input sanitization.
-        try {
-          int payint = Integer.parseInt(payload);
-          if (payint < 10 || payint > 10000) prediction = "R4-5";
-        } catch (Exception e) {
-          prediction = "NaN";
-        }
-
-        try {
-          Listing pListing =
-              new Listing(
-                  id,
-                  Integer.toString(id),
-                  "A kinda short description because the title is now a 5 digit integer :)",
-                  Integer.parseInt(payload),
-                  LocalDate.now(),
-                  404);
-        } catch (NumberFormatException e) {
-          error = "NaN";
-        } catch (IllegalArgumentException e) {
-          error = e.getMessage();
-        }
-
-        Assertions.assertEquals(prediction, error);
-        id++;
+      // predict if the payload will throw an error due to current input sanitization.
+      try {
+        int payint = Integer.parseInt(payload);
+        if (payint < 10 || payint > 10000) prediction = "R4-5";
+      } catch (Exception e) {
+        prediction = "NaN";
       }
-    } else Assertions.assertTrue(true);
+
+      try {
+        Listing pListing =
+            new Listing(
+                id,
+                Integer.toString(id),
+                "A kinda short description because the title is now a 5 digit integer :)",
+                Integer.parseInt(payload),
+                LocalDate.now(),
+                404);
+      } catch (NumberFormatException e) {
+        error = "NaN";
+      } catch (IllegalArgumentException e) {
+        error = e.getMessage();
+      }
+
+      Assertions.assertEquals(prediction, error);
+      id++;
+    }
   }
 }
